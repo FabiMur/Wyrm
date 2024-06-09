9d47b0eb137101b66d4dcef0e70b71b616c6dc98
Fabián Murgado <fabianmurgado@gmail.com>
2024-06-10 01:38:09 +0200

    First image generated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let image_width = 256;
+    let image_height = 256;
+
+    println!("P3\n{} {}\n255", image_width, image_height);
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0.0;
+
+            let ir = (255.999 * r) as u8;
+            let ig = (255.999 * g) as u8;
+            let ib = (255.999 * b) as u8;
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
